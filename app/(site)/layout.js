02e91a387520d62f7113c0c3d1a55681ce755481
Arizli <arielromadhon7@gmail.com>
2023-09-16 00:01:03 +0700

    first commit

---
+++ app/(site)/layout.js
@@ -0,0 +1,10 @@
+export const metadata = {
+    title: 'Project Management',
+    description: 'Home Page of Project Management',
+}
+
+export default function SiteLayout({ children }) {
+    return (
+        <main>{children}</main>
+    )
+}
---
+++ app/(site)/page.js
@@ -0,0 +1,14 @@
+import NavbarHome from "./components/nav";
+
+export default function Home() {
+    return (
+        <main>
+            <header>
+                <NavbarHome />
+            </header>
+            <div>
+                Hellooo
+            </div>
+        </main>
+    );
+}
